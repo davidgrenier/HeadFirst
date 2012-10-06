4681b866da053fd37364aa6c2609823a912ee725
David Grenier <davidgrenier@gmail.com>
2012-10-06 17:54:28 -0400

    ReflectedDefinition <> mutually rec fns

--- HeadFirst/HeadFirstLounge.fs
+++ HeadFirst/HeadFirstLounge.fs
@@ -1,51 +1,56 @@
-﻿[<ReflectedDefinition>]
-module HeadFirst.Lounge
+﻿module HeadFirst.Lounge
 
 open IntelliFactory.WebSharper
 open IntelliFactory.WebSharper.Html
 
+[<ReflectedDefinition>]
 let core = Div []
 
+[<ReflectedDefinition>]
 let directions = []
 
-let rec elixirs () =
-    let drink header img description =
+[<ReflectedDefinition>]
+let body =
+    let rec elixirs () =
+        let drink header img description =
+            [
+                H2 [Text header]
+                P [
+                    Img [Src ("images/" + img)] :> IPagelet
+                    Text description
+                ]
+            ]            
         [
-            H2 [Text header]
+            yield H1 [Text "Our Elixirs"]
+            yield! drink "Green Tea Cooler" "green.jpg"
+                "Chock full of vitamins and minerals, this elixir combines the healthful benefits of green tea with a twist of chamomile blossoms and ginger root."
+            yield! drink "Rapsberry Ice Concentration" "lightblue.jpg"
+                "Combining raspberry juice with lemon grass, citrus peel adn rosehips, this icy drink will make your mind feel clear and crips."
+            yield! drink "Blueberry Bliss Elixir" "blue.jpg"
+                "Blueberries and cherry essence mixed into a base of elderflower herb tea will put you in a relaxed state of bliss in no time."
+            yield! drink "Cranberry Antioxidant Blast" "red.jpg"
+                "Wake up to the flavors of cranberry and hibiscus in this vitamin C rich elixir."
+            yield A [HRef "#"; Text "Back to the Lounge"] |>! OnClick (fun _ _ -> core =< lounge ())
+        ]
+    
+    and lounge () =
+        [
+            H1 [Text "Welcome to the New and Improved Head First Lounge"]
+            Img [Src "Images/drinks.gif"]
             P [
-                Img [Src ("images/" + img)] :> IPagelet
-                Text description
+                Text "Join us any evening for refreshing "
+                A [HRef "#Elixirs"; Text "elixirs"] :> _ |>! OnClick (fun _ _ -> core =< elixirs ())
+                Text ", conversation and maybe a game or two of Dance Dance Revolution. Wireless access is always provided; BYOWS (Bring Your Own Web Server)."
             ]
-        ]            
-    [
-        yield H1 [Text "Our Elixirs"]
-        yield! drink "Green Tea Cooler" "green.jpg"
-            "Chock full of vitamins and minerals, this elixir combines the healthful benefits of green tea with a twist of chamomile blossoms and ginger root."
-        yield! drink "Rapsberry Ice Concentration" "lightblue.jpg"
-            "Combining raspberry juice with lemon grass, citrus peel adn rosehips, this icy drink will make your mind feel clear and crips."
-        yield! drink "Blueberry Bliss Elixir" "blue.jpg"
-            "Blueberries and cherry essence mixed into a base of elderflower herb tea will put you in a relaxed state of bliss in no time."
-        yield! drink "Cranberry Antioxidant Blast" "red.jpg"
-            "Wake up to the flavors of cranberry and hibiscus in this vitamin C rich elixir."
-        yield A [HRef "#"; Text "Back to the Lounge"] |>! OnClick (fun _ _ -> core =< lounge ())
-    ]
-
-and lounge () =
-    [
-        H1 [Text "Welcome to the New and Improved Head First Lounge"]
-        Img [Src "Images/drinks.gif"]
-        P [
-            Text "Join us any evening for refreshing "
-            A [HRef "#Elixirs"; Text "elixirs"] :> _ |>! OnClick (fun _ _ -> core =< elixirs ())
-            Text ", conversation and maybe a game or two of Dance Dance Revolution. Wireless access is always provided; BYOWS (Bring Your Own Web Server)."
+            H2 [Text "Directions"]
+            P [
+                Text "You'll find us right in the center of downtown Webville. If you need help finding us, check out our "
+                A [HRef "#Directions"; Text "detailed directions"] :> _ |>! OnClick (fun _ _ -> core =< directions)
+                Text ". Come join us!"
+            ]
         ]
-        H2 [Text "Directions"]
-        P [
-            Text "You'll find us right in the center of downtown Webville. If you need help finding us, check out our "
-            A [HRef "#Directions"; Text "detailed directions"] :> _ |>! OnClick (fun _ _ -> core =< directions)
-            Text ". Come join us!"
-        ]
-    ]
 
-and body () =
-    core -< lounge ()
+    and body () =
+        core -< lounge ()
+    
+    body
