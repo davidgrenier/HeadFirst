3830647cda508c7c66c2237fae341ae7a7e1ac39
David Grenier <davidgrenier@gmail.com>
2012-10-10 22:14:23 -0400

    Removed extra space before quotes.

--- HeadFirst/Server.fs
+++ HeadFirst/Server.fs
@@ -31,10 +31,10 @@ module Tonys =
     let journalEntries() =
         [
             Entry (Date(2012, 8, 20), Some "Segway2", "Well I made it 1200 miles already, and I passed through some interesting places on teh way: Walla Walla, WA, Magic City, ID, Bountiful, UT, Last Chance, CO, Why, AZ and Truth or Consequences, NM.")
-            Entry (Date(2012, 7, 14), None, "I saw some Burma Shave style signs on the side of the road today: \"Passing cars,
+            Entry (Date(2012, 7, 14), None, "I saw some Burma Shave style signs on the side of the road today:\"Passing cars,
 When you can't see,
 May get you,
 A glimpse,
 Of eternity.\" I definitely won't be passing any cars.")
-            Entry (Date(2012, 6, 2), Some "Segway1", "My first day of the trip! I can't believe finally got everything packed and ready to go. Because I'm on a Segway, I wasn't able to bring a whole lot with me: cell phone, iPod, digital camera, and a protein bar. Just the essentials. As Lao Tzu would have said, \"A journey of a thousand miles begins with one Segay.\"")
+            Entry (Date(2012, 6, 2), Some "Segway1", "My first day of the trip! I can't believe finally got everything packed and ready to go. Because I'm on a Segway, I wasn't able to bring a whole lot with me: cell phone, iPod, digital camera, and a protein bar. Just the essentials. As Lao Tzu would have said,\"A journey of a thousand miles begins with one Segay.\"")
         ]
