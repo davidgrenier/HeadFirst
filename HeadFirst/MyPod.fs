c760ba047f28b973e42d3f8379766d5e85ab7376
David Grenier <davidgrenier@gmail.com>
2012-10-21 18:49:50 -0400

    Halved the picture size

--- HeadFirst/MyPod.fs
+++ HeadFirst/MyPod.fs
@@ -8,17 +8,17 @@ let body () =
     Div [
         h1 "Welcome to myPod"
         P [Text "Welcome to the place to show off your iPod, wherever you might be.
-        Wanna join the fun? All you need is any iPod from the early classic 
-        iPod to the latest iPod Nano, the smallest iPod Shuffle to the largest 
-        iPod Video, and a digital camera. Just take a snapshot of your iPod in
-        your favorite location and we'll be glad to post it on myPod. So, what 
-        are you waiting for?"
+            Wanna join the fun? All you need is any iPod from the early classic 
+            iPod to the latest iPod Nano, the smallest iPod Shuffle to the largest 
+            iPod Video, and a digital camera. Just take a snapshot of your iPod in
+            your favorite location and we'll be glad to post it on myPod. So, what 
+            are you waiting for?"
         ]
 
         h2 "Seattle, Washington"
         P [Text "Me and my iPod in Seattle! You can see the Space Needle. You can't see the 628 coffee shops."]
 
         P [
-            Img [Src "images/seattle.jpg"; Width "100%"]
+            Img [Src "images/seattle-half.jpg"]
         ]
     ]
